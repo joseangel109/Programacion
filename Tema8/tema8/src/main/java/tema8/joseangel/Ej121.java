0ae3a0f3788bdb2eb53cadd48464ef023390e5c9
Jose Angel Albaladejo Sánchez <joseangelalbaladejosanchez@gmail.com>
2024-01-12 10:30:03 +0100

    12/01/24

--- Tema8/tema8/src/main/java/tema8/joseangel/Ej121.java
+++ Tema8/tema8/src/main/java/tema8/joseangel/Ej121.java
@@ -2,8 +2,8 @@ package tema8.joseangel;
 
 import java.util.Scanner;
 
-public class Ej12 {
-    public static void main(String[] args, Object opcion) {
+public class Ej121 {
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int array1[]= new int [10];
         String menu;
@@ -14,7 +14,7 @@ public class Ej12 {
             menu= teclado.nextLine();
             switch (menu) {
                 case "a":
-
+                        System.out.println("Has elegido la opcion A");
                     break;
                 case "b":
                         System.out.println("Dame el valor de V");
