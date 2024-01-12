7ebb5af62622596428a914111687b1e80cc9688e
Jose Angel Albaladejo Sánchez <joseangelalbaladejosanchez@gmail.com>
2024-01-12 10:13:44 +0100

    ej12
    eje12

--- Tema8/tema8/src/main/java/tema8/joseangel/Ej11.java
+++ Tema8/tema8/src/main/java/tema8/joseangel/Ej11.java
@@ -2,59 +2,21 @@ package tema8.joseangel;
 
 import java.util.Scanner;
 
-public class Ej10 {
+public class Ej11 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-
-        // Pedir el valor N numero alturas
-        System.out.print("Introduce el número de alturas (N): ");
-        int N = teclado.nextInt();
-
-        // Crear un array para almacenar las alturas
-        double[] alturas = new double[N];
-
-        // Pedir las alturas
-        for (int i = 0; i < N; i++) {
-            System.out.print("Introduce la altura para la persona " + (i + 1) + " (en metros): ");
-            alturas[i] = teclado.nextDouble();
+        int auxiliar=100;
+        int array1[] = new int[100];
+        int array2[] = new int[100];
+        for (int i = 0; i < array1.length; i++) {
+            array1[i]=i+1;
+            System.out.println(array1[i]);
         }
-
-        // Calcular la altura media
-        double sumaAlturas = 0;
-        for (double altura : alturas) {
-            sumaAlturas += altura;
+        for (int i = 0; i < array2.length; i++) {
+            auxiliar-=1;
+            array2[i]=array1[auxiliar];
+            System.out.println(array2[i]);
         }
-        double alturaMedia = sumaAlturas / N;
-
-        // Calcular la altura máxima y mínima
-        double alturaMaxima = alturas[0];
-        double alturaMinima = alturas[0];
-        for (int i = 1; i < N; i++) {
-            if (alturas[i] > alturaMaxima) {
-                alturaMaxima = alturas[i];
-            }
-            if (alturas[i] < alturaMinima) {
-                alturaMinima = alturas[i];
-            }
-        }
-
-        // Contar cuántas personas miden por encima y por debajo de la media
-        int personasPorEncima = 0;
-        int personasPorDebajo = 0;
-        for (double altura : alturas) {
-            if (altura > alturaMedia) {
-                personasPorEncima++;
-            } else if (altura < alturaMedia) {
-                personasPorDebajo++;
-            }
-        }
-
-        // Mostrar los resultados
-        System.out.println("Altura media: " + alturaMedia + " metros");
-        System.out.println("Altura máxima: " + alturaMaxima + " metros");
-        System.out.println("Altura mínima: " + alturaMinima + " metros");
-        System.out.println("Personas por encima de la media: " + personasPorEncima);
-        System.out.println("Personas por debajo de la media: " + personasPorDebajo);
-
+    teclado.close();
     }
 }
---
+++ Tema8/tema8/src/main/java/tema8/joseangel/Ej12.java
@@ -0,0 +1,36 @@
+package tema8.joseangel;
+
+import java.util.Scanner;
+
+public class Ej12 {
+    public static void main(String[] args, Object opcion) {
+        Scanner teclado = new Scanner(System.in);
+        int array1[]= new int [10];
+        String menu;
+        do {
+            System.out.println("a) Mostrar valores");
+            System.out.println("b) Introducir valores");
+            System.out.println("c) Salir");
+            menu= teclado.nextLine();
+            switch (menu) {
+                case "a":
+
+                    break;
+                case "b":
+                        System.out.println("Dame el valor de V");
+                        int v = teclado.nextInt();
+                        System.out.println("Dame el valor de P");
+                        int p = teclado.nextInt();
+                    break;
+                case "c":
+                        System.out.println("Has elegido Salir");
+                    break;
+            
+                default:
+                    System.out.println("ELIGE UNA OPCION VALIDA");
+                    break;
+            }
+        } while (!menu.equals("c"));
+    teclado.close();
+    }
+}
