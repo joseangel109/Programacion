a003f90ec4790d83ca621585c45784b165118f71
Jose Angel Albaladejo Sánchez <joseangelalbaladejosanchez@gmail.com>
2024-01-29 12:50:11 +0100

    29/1/24

--- Tema8/tema8/src/main/java/tema8/buscaminas/practica.java
+++ Tema8/tema8/src/main/java/tema8/buscaminas/practica.java
@@ -1,11 +1,9 @@
 package tema8.buscaminas;
-
-import java.util.ArrayList;
-//Crear un tablero para un buscaminas
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class practica {
+
     public static void main(String[] args) {
         int ancho = 0;
         int alto = 0;
@@ -16,24 +14,41 @@ public class practica {
         ancho = teclado.nextInt();
         System.out.println("Dime el numero de minas");
         int minas = teclado.nextInt();
-        practica.crearTablero(int ancho,int alto,int minas);
-        practica.mostrarTablero();
+        practica.crearTablero(ancho, alto, minas);
     }
-    public void crearTablero(int alto, int ancho, int minas){
+
+    public static void crearTablero(int alto, int ancho, int minas) {
         int matriz[][] = new int[ancho][alto];
-        for (int i = 0; i == minas; i++) {
+        for (int i = 0; i < minas; i++) {
             int posicion1 = ThreadLocalRandom.current().nextInt(0, ancho);
             int posicion2 = ThreadLocalRandom.current().nextInt(0, alto);
-            matriz[posicion1][posicion2] = 1;
-           }
+            matriz[posicion1][posicion2] = -1;
         }
-    public void mostrarTablero(int alto,int ancho, int minas){
-        int matriz[][] = new int[ancho][alto];
+        practica.mostrarTablero(matriz);
+    }
+
+    public static void mostrarTablero(int matriz[][]) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (matriz[i][j] == -1) {
+                    if (matriz[i][j] != -1) matriz[i][j]++;
+                    if (matriz[i][j - 1] != -1) matriz[i][j - 1]++;
+                    if (matriz[i][j + 1] != -1) matriz[i][j + 1]++;
+                    if (matriz[i - 1][j - 1] != -1) matriz[i - 1][j - 1]++;
+                    if (matriz[i - 1][j] != -1) matriz[i - 1][j]++;
+                    if (matriz[i - 1][j + 1] != -1) matriz[i - 1][j + 1]++;
+                    if (matriz[i + 1][j - 1] != -1) matriz[i + 1][j - 1]++;
+                    if (matriz[i + 1][j] != -1)  matriz[i + 1][j]++;
+                    if (matriz[i + 1][j + 1] != -1) matriz[i + 1][j + 1]++;
+                }
+            }
+        }
         for (int[] i : matriz) {
             for (int js : i) {
-                System.out.printf("%3d", js);   
+                System.out.printf("%3d", js);
             }
             System.out.println();
-           }
+        }
     }
+
 }
