e9aec45c6ddf1f4f6a03991b3a876471599dd444
joseangel109 <150005995+joseangel109@users.noreply.github.com>
2024-02-12 21:03:37 +0100

    12/2/24

--- Tema9/Tema9POOII/src/Ej2/Loro.java
+++ Tema9/Tema9POOII/src/Ej2/Loro.java
@@ -1,6 +1,82 @@
 package Ej2;
 
-public class Loro {
+import java.time.LocalDate;
+
+public class Loro extends Aves {
     String origen;
     boolean habla;
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public boolean isHabla() {
+        return habla;
+    }
+
+    public void setHabla(boolean habla) {
+        this.habla = habla;
+    }
+
+    public Loro(String nombre, int edad, boolean estado, LocalDate fechaNacimiento, boolean pico, boolean vuela,
+            String origen, boolean habla) {
+        super(nombre, edad, estado, fechaNacimiento, pico, vuela);
+        this.origen = origen;
+        this.habla = habla;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((origen == null) ? 0 : origen.hashCode());
+        result = prime * result + (habla ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Loro other = (Loro) obj;
+        if (origen == null) {
+            if (other.origen != null)
+                return false;
+        } else if (!origen.equals(other.origen))
+            return false;
+        if (habla != other.habla)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Loro [origen=" + origen + ", habla=" + habla + super.toString() + "]";
+    }
+
+    public void saluda() {
+        System.out.println(toString());
+
+    }
+
+    public void volar() {
+        if (vuela)
+            System.out.println("Tu animal puede volar");
+        else
+            System.out.println("Tu animal no puede volar");
+
+    }
+
+    public void muestra() {
+        System.out.println(toString());
+
+    }
 }
