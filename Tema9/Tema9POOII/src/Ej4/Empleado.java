1884e142a9a82675ea795ca79a4b7dbf9738098a
Jose Angel Albaladejo Sánchez <joseangelalbaladejosanchez@gmail.com>
2024-02-13 13:20:53 +0100

    13/2/24

--- Tema9/Tema9POOII/src/Ej4/Empleado.java
+++ Tema9/Tema9POOII/src/Ej4/Empleado.java
@@ -1,3 +1,130 @@
+package Ej4;
+
 public class Empleado {
-    
+    final String nombre;
+    final String dni;
+    static float sueldoBruto;
+    int edad;
+    int telefono;
+    String direccion;
+
+    public Empleado(String nombre, String dni, float sueldoBruto, int edad, int telefono, String direccion) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.sueldoBruto = sueldoBruto;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public float getSueldoBruto() {
+        return sueldoBruto;
+    }
+
+    public void setSueldoBruto(float sueldoBruto) {
+        this.sueldoBruto = sueldoBruto;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+        result = prime * result + Float.floatToIntBits(sueldoBruto);
+        result = prime * result + edad;
+        result = prime * result + telefono;
+        result = prime * result + ((direccion == null) ? 0 : direccion.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Empleado other = (Empleado) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (dni == null) {
+            if (other.dni != null)
+                return false;
+        } else if (!dni.equals(other.dni))
+            return false;
+        if (Float.floatToIntBits(sueldoBruto) != Float.floatToIntBits(other.sueldoBruto))
+            return false;
+        if (edad != other.edad)
+            return false;
+        if (telefono != other.telefono)
+            return false;
+        if (direccion == null) {
+            if (other.direccion != null)
+                return false;
+        } else if (!direccion.equals(other.direccion))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado [nombre=" + nombre + ", dni=" + dni + ", sueldoBruto=" + sueldoBruto + ", edad=" + edad
+                + ", telefono=" + telefono + ", direccion=" + direccion + "]";
+    }
+
+    public void muestra() {
+        System.out.println(toString());
+    }
+
+    public void calculaNeto(float sueldoBruto) {
+        int irpf = 0;
+        int sueldoAnual = sueldoBruto * 12;
+        if (sueldoAnual < 12000)
+            irpf = 20;
+        if (sueldoAnual > 12000 && sueldoAnual < 25000)
+            irpf = 30;
+        else
+            irpf = 40;
+
+        int sueldoNeto = sueldoBruto - ((sueldoBruto * irpf) / 100);
+
+    }
+
 }
--- Tema9/Tema9POOII/src/Ej4/Empresa.java
+++ Tema9/Tema9POOII/src/Ej4/Empresa.java
@@ -1,3 +1,91 @@
+package Ej4;
+
+import java.util.ArrayList;
+
 public class Empresa {
-    
+    final String nombre;
+    final String CIF;
+    int telefono;
+    String direccion;
+    int empleados = 0;
+
+    static ArrayList<Empleado> listaEmpleados = new ArrayList<>();
+
+    public Empresa(String nombre, String cIF, int telefono, String direccion, int empleados) {
+        this.nombre = nombre;
+        CIF = cIF;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.empleados = empleados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCIF() {
+        return CIF;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(int empleados) {
+        this.empleados = empleados;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa [nombre=" + nombre + ", CIF=" + CIF + ", telefono=" + telefono + ", direccion=" + direccion
+                + ", empleados=" + empleados + "]";
+    }
+
+    public static void añadirEmpleados(Empleado e) {
+        listaEmpleados.add(e);
+    }
+
+    public static void eliminarEmpleados(Empleado e) {
+        listaEmpleados.remove(e);
+    }
+
+    public static void mostrarEmpleados() {
+        System.out.println(listaEmpleados.toString());
+    }
+
+    public void mostrarDNIsueldoByN() {
+
+    }
+
+    public void calcularBrutoTodos(ArrayList<Empleado> listaEmpleados) {
+        float sueldos = 0;
+        for (int i = 0; i < array.length; i++) {
+            sueldos += Empleado.sueldoBruto;
+        }
+        System.out.println("Los sueldos brutos de los empleados " + sueldos);
+    }
+
+    public void calculaNetoTodos(ArrayList<Empleado> listaEmpleados) {
+        float sueldos = 0;
+        for (int i = 0; i < array.length; i++) {
+            sueldos += Empleado.sueldoNeto;
+        }
+        System.out.println("Los sueldos netos de los empleados " + sueldos);
+    }
 }
