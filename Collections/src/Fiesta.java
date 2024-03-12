5c7fa750a297dd1e0dfb0ec5555b6351d5e1a4ea
Jose Angel Albaladejo Sánchez <joseangelalbaladejosanchez@gmail.com>
2024-03-12 12:43:23 +0100

    12/3/24

--- Collections/src/Fiesta.java
+++ Collections/src/Fiesta.java
@@ -7,8 +7,7 @@ public class Fiesta {
         HashSet <Personas> fiesta = new HashSet<>();
         int mediaEdad=0;
         int mayor=0;
-        fiesta.add(new Personas("Juan", 33444555, "janu..", LocalDate.of(2002, Month.JUNE, 3)));
-        fiesta.add(new Personas("Juan", 111222333, "juan..", LocalDate.of(1972, 3, 23)));
+        fiesta.add(new Personas("saksa", 33444555, "janu..", LocalDate.of(2002, Month.JUNE, 3)));
         fiesta.add(new Personas("Juan", 111222333, "juan..", LocalDate.of(1972, 3, 23)));
         fiesta.add(new Personas("Pablo", 99965656, "ash..", LocalDate.of(1999, Month.DECEMBER, 20)));
         fiesta.add(new Personas("Jose", 123456789, "jsss..", LocalDate.of(1992, Month.JANUARY, 23)));
@@ -28,7 +27,7 @@ public class Fiesta {
         System.out.println("-----------------------------------------------");
         System.out.println("La media de edad es de: "+mediaEdad/fiesta.size()); 
         System.out.println("------------------------------------------");
-        System.out.println("El mayor es: ");
+        System.out.println("El mayor es: "+mayor);
     }
 //El orden que tiene es en el que se han añadido
 
