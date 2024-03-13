67b5aedb5fec412120f1423016e588a4b81ffe10
Jose Angel Albaladejo Sánchez <joseangelalbaladejosanchez@gmail.com>
2024-03-13 12:10:30 +0100

    13/3/24

--- Collections/src/Equipo.java
+++ Collections/src/Equipo.java
@@ -1,5 +1,87 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Equipo {
     public static void main(String[] args) {
+        HashMap<Integer, Jugador> listaEquipo = new HashMap();
+        listaEquipo.put(1, new Jugador("juan", 100));
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce 10 para salir");
+        int salir;
+        do {
+            System.out.println("Introduce 10 si quieres salir del programa, nada si quieres seguir");
+            salir=teclado.nextInt();
+            mostrarMenu();
+            int opcion = teclado.nextInt();
+             
+            elegirOpcion(opcion, listaEquipo, teclado);   
+             
+        } while (salir==10);
+        
+    }
 
+    static void mostrarMenu() {
+        System.out.println("Elige que quieres hacer");
+        System.out.println("Añadir jugador (1)");
+        System.out.println("Eliminar jugador (2)");
+        System.out.println("Consultar Salario (3)");
+        System.out.println("Aumentar Salario un 10% (4)");
+        System.out.println("Introduce 10 para salir");
+    }
+
+    static void elegirOpcion(int opcion, HashMap listaEquipo, Scanner teclado) {
+    
+        switch (opcion) {
+            case 1:
+                System.out.println("Has elegido añadir jugador");
+                añadirJugador(listaEquipo, teclado);
+                break;
+            case 2:
+                System.out.println("Has elegido eliminar jugador");
+                eliminarJugador(listaEquipo, teclado);
+                break;
+            case 3:
+                System.out.println("Has elegido consultar salario");
+                mostrarInfoJugador(listaEquipo, teclado);
+                break;
+            case 4:
+                System.out.println("Has elegido aumentar salario un 10%");
+                aumentarSalario(listaEquipo, teclado);
+                break;
+            default:
+                System.out.println("Numero introducido incorrecto");
+                break;
+        }    
+    }
+
+    static void añadirJugador(HashMap listaEquipo, Scanner teclado) {
+        System.out.println("Vas a añadir un jugador");
+        
+    }
+
+    static void eliminarJugador(HashMap listaEquipo, Scanner teclado) {
+        System.out.println("Dime el ID del jugador que quieres eliminar");
+        int eliminarID=teclado.nextInt();
+        if(listaEquipo.containsKey(eliminarID))
+            listaEquipo.remove(eliminarID);
+        else
+            System.out.println("No se ha encontrado ningun jugador con ese ID");
+    }
+
+    static void mostrarInfoJugador(HashMap listaEquipo, Scanner teclado) {
+        System.out.println("Dime el ID del jugador");
+        int buscarID = teclado.nextInt();
+        if(listaEquipo.containsKey(buscarID))
+            listaEquipo.get(buscarID);
+            System.out.println(listaEquipo.get(buscarID));
+    }
+
+    static void aumentarSalario(HashMap listaEquipo, Scanner teclado) {
+        System.out.println("Dime el ID del jugador que quieres aumentar");
+        int buscarID = teclado.nextInt();
+        if(listaEquipo.containsKey(buscarID))
+            listaEquipo.get(buscarID);
+        System.out.println( );
+            System.out.println(listaEquipo.get(buscarID));
     }
 }
--- Collections/src/Jugador.java
+++ Collections/src/Jugador.java
@@ -2,4 +2,65 @@ public class Jugador {
     String nombre;
     double salario;
 
+
+    public Jugador(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        return result;
+    }
+
+
+    
+    @Override
+    public String toString() {
+        return "Jugador " + nombre + ", salario=" + salario;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Jugador other = (Jugador) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        return true;
+    }
+    
+    
 }
